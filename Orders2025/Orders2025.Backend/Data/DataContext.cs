e40377137f8b7e06f6ed596fd1e3564cb05a2d1b
Emilio Barrera <barrera_emilio@yahoo.es>
2025-09-14 12:51:42 -0500

    DB creation and first  Entity Country

---
+++ Orders2025/Orders2025.Backend/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders2025.Shared.Entities;
+
+namespace Orders2025.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(X => X.Name).IsUnique();
+    }
+}
