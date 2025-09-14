54587470beecde354ddc6fef149ce3e3e7eef0ff
Emilio Barrera <barrera_emilio@yahoo.es>
2025-09-14 14:06:05 -0500

    Put and get for countries

---
+++ Orders2025/Orders2025.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders2025.Backend.Data;
+using Orders2025.Shared.Entities;
+
+namespace Orders2025.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public CountriesController(DataContext dataContext)
+    {
+        this._dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _dataContext.Countries.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Country country)
+    {
+        _dataContext.Countries.Add(country);
+        await _dataContext.SaveChangesAsync();
+        return Ok(country);
+    }
+}
